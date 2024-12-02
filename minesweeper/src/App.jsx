857e0dcc8ba33e9186fed7fb0cb30b6a466825eb
dimmestsalmon <appelnephi@gmail.com>
2024-12-02 16:49:03 -0500

    game logic almost complete

--- minesweeper/src/App.jsx
+++ minesweeper/src/App.jsx
@@ -12,23 +12,49 @@ function App() {
     let mines = []
     for(let j=1; j<=10; j++){
       mines.push(Math.floor(Math.random() * 100) + 1)
-      mines.sort()
-
+      mines.sort((a, b) => a - b)
     }
-    console.log(mines)
+
     for (let i=1; i <= 100; i++){
       if(mines[mineCount]===i){
-      board.push(<div key = {i} className = 'square' id = 'mine'></div>)
+      board.push(<div key = {i} className = 'square mine' id = {`square${i}`} onClick = {() => checkSquare(event.target.className)}>mine</div>)
       mineCount++;
-      console.log(mineCount)
       } else{
-        board.push(<div key = {i} className = 'square' id = {i}></div>)
+        board.push(<div key = {i} className = 'square' id = {`square${i}`} onClick = {() => checkSquare(event.target.className)}></div>)
       }
-
     }
     return (
       board
     )
+  }
+
+  const checkSquare = (targetCheck) => {
+    if(targetCheck === 'square mine'){
+      console.log('Game Over')
+    } else{
+      let oldId = event.target.id
+    let newId = oldId.match(/(\d+)/)[0]
+      noMines(newId)
+  }
+  }
+
+  const noMines = (newId) => {
+    let minesAround = 0;
+    document.querySelector(`#${event.target.id}`).style.backgroundColor = 'white'
+    for(let i=-11; i<=11; i++){
+
+      if(i === -9 || i === -10 || i === -11 || i === -1 || i === 1 || i === 9 || i === 10 || i === 11){
+        if(document.querySelector(`#square${parseInt(newId) + i}`).className != 'square mine'){
+          document.querySelector(`#square${parseInt(newId) + i}`).style.backgroundColor = 'white'
+          // noMines(parseInt(newId)+i)
+        } else {
+          minesAround ++;
+          console.log(minesAround)
+          document.querySelector(`#${event.target.id}`).innerHTML = minesAround
+        }
+      }
+
+    }
   }
 
   return (
